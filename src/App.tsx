215674bd41d6b312ad3e9fa79cc9727fc25ebc67
yjc3345 <121676669+yjc3345@users.noreply.github.com>
2026-09-10 10:07:52 +0900

    100코인 버튼 야삐

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
+<button onClick={() => setCoins(100)} style={{position: 'fixed', top: 10, left: 10, zIndex: 9999, background: 'red', color: 'white', padding: '10px'}}>
+  🧪 치트키: 코인 100개 채우기
+</button>
 
 function App() {
   return (
